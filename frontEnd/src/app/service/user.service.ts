cf6926ee40d26c6ad86eae276fedaa9d9a1d777d
sezarmbar <sezar_mbar@yahoo.com>
2017-08-10 00:39:08 +0200

    cleane

--- frontEnd/src/app/service/user.service.ts
+++ frontEnd/src/app/service/user.service.ts
@@ -1,23 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Headers } from '@angular/http';
-import { ApiService2 } from './api.service';
-import { ConfigService } from './config.service';
+import {Injectable} from '@angular/core';
+import {Headers} from '@angular/http';
+import {ApiService2} from './api.service';
+import {ConfigService} from './config.service';
 
 @Injectable()
 export class UserService {
 
   currentUser;
 
-  constructor(
-    private apiService: ApiService2,
-    private config: ConfigService
-  ) { }
+  constructor(private apiService : ApiService2, private config : ConfigService,
+    // private loginGuard : LoginGuard,
+    //  private createUserPage : CreateUserPage,
+      // private adminPage : AdminPage
+    ) {}
 
   initUser() {
-    const promise = this.apiService.anonGet(this.config.refresh_token_url).toPromise()
+    const promise = this
+      .apiService
+      .anonGet(this.config.refresh_token_url)
+      .toPromise()
       .then(res => {
         if (res.access_token !== null) {
-          return this.getMyInfo().toPromise()
+          return this
+            .getMyInfo()
+            .toPromise()
             .then(user => {
               this.currentUser = user;
             });
@@ -29,16 +35,24 @@ export class UserService {
 
   getMyInfo() {
 
-    return this.apiService.getUser(this.config.whoami_url).map(user => this.currentUser = user);
+    return this
+      .apiService
+      .getUser(this.config.whoami_url)
+      .map(user => this.currentUser = user);
   }
 
   getAll() {
-    return this.apiService.get(this.config.users_url);
+    return this
+      .apiService
+      .get(this.config.users_url);
   }
   getAllUsers() {
     console.log('"get ALL : "');
-    this.apiService.getAllUsers(this.config.users_url).subscribe((data) => {
-      return data;
-    }, (error) => console.log(error));
+    this
+      .apiService
+      .getAllUsers(this.config.users_url)
+      .subscribe((data) => {
+        return data;
+      }, (error) => console.log(error));
   }
 }
