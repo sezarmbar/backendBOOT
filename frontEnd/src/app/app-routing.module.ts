1440ccd4c4ed592826257bb72c026452bc54d7c4
sezarmbar <sezar_mbar@yahoo.com>
2017-08-10 10:43:16 +0200

    add HomeComponent

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -11,6 +12,7 @@ import { CreateUsersComponent } from "./pages/create-users";
 const routes: Routes = [
     { path: 'r/:id', component: RatingPageComponent },
     { path: '404', component: ErrorPageComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
     { path: 'admin', component: AdminRatingPageComponent, canActivate: [AdminPage] },
     { path: 'creatUsers', component: CreateUsersComponent ,canActivate:[CreateUserPage]},
