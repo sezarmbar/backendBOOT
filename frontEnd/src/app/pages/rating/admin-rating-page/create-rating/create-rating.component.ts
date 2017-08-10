fb48c129f0e16c3973ad5ad2bcad92264938c76c
sezarmbar <sezar_mbar@yahoo.com>
2017-08-10 19:56:38 +0200

    cleane

--- frontEnd/src/app/pages/rating/admin-rating-page/create-rating/create-rating.component.ts
+++ frontEnd/src/app/pages/rating/admin-rating-page/create-rating/create-rating.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
 import { Rating,RatingService } from '../../';
 
 @Component({
@@ -14,25 +14,51 @@ export class CreateRatingComponent implements OnInit {
   form: FormGroup;
   submitted = false;
   requestProcessing = false;
-
   statusCode: number;
-  constructor(private ratingService: RatingService, private formBuilder: FormBuilder, ) { }
+
+  ratingName:FormControl;
+  waitingTime:FormControl;
+  description:FormControl;
+  active:FormControl;
+
+  constructor(private ratingService: RatingService, private formBuilder: FormBuilder, ) { 
+     this.createForm();
+  }
   ngOnInit() {
+    // this.form = this.formBuilder.group({
+    //   ratingName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(16)])],
+    //   waitingTime: ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(3)])],
+    //   description: ['', Validators.compose([Validators.minLength(3), Validators.maxLength(64)])],
+    //   active: ['']
+    // });
+this.createForm();
+  }
+  createForm(){
+    this.ratingName = new FormControl('',[Validators.required, Validators.minLength(3)]);
+    this.waitingTime = new FormControl('',Validators.required);
+    this.description = new FormControl('',Validators.required);
+    this.active = new FormControl('');
 
     this.form = this.formBuilder.group({
-      ratingName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(16)])],
-      waitingTime: ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(3)])],
-      description: ['', Validators.compose([Validators.minLength(3), Validators.maxLength(64)])],
-      active: []
+      ratingName: this.ratingName,
+      waitingTime: this.waitingTime,
+      description: this.description,
+      active: this.active
     });
 
   }
+  showForm:boolean = true;
   onSubmit() {
     this.submitted = true;
     const creatRD = this.form.value;
     this.rating = new Rating(null, creatRD.ratingName, creatRD.description, 0, 0, 0, 0, 0, null, creatRD.active, creatRD.waitingTime);
     this.createRating(this.rating);
-    this.form.reset();
+    
+    this.showForm = false;
+    setTimeout(() => {
+    this.createForm()
+      this.showForm = true;
+    });
   }
 
   createRating(rating: Rating) {
